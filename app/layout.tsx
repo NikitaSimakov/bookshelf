0477e058bfcb44296fe8d915238332b5dadb4dfa
NikitaSimakov <mishaaniskin@gmail.com>
2024-01-16 20:00:21 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,8 @@
 "use client";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 
 import "./globals.scss";
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Bookshelf",
-//   description: "Here we are reading",
-//   icons: {
-//     icon: [
-//       {
-//         media: "(prefers-color-scheme: light)",
-//         url: "/favicon.ico",
-//         href: "/favicon.ico",
-//       },
-//     ],
-//   },
-// };
 
 export default function RootLayout({
   children,
