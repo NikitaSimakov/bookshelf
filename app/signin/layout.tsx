c59e58f79b71e0fbceab92c6a82b403b713a7ff6
NikitaSimakov <mishaaniskin@gmail.com>
2024-01-17 21:02:58 +0300

    Sign in & Sign up styled pages (light theme)

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,11 +1,16 @@
 import { Metadata } from "next";
 import SignIn from "./page";
+import style from "./signin.module.scss";
 
 export const metadata: Metadata = {
   title: "Bookshelf | Sign in",
 };
 const SignInLayout = () => {
-  return <SignIn />;
+  return (
+    <section className={style.container}>
+      <SignIn />
+    </section>
+  );
 };
 
 export default SignInLayout;
