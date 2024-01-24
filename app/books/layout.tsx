ec2f8c3232d9cb0b2b966001b713bf17e8d333b0
NikitaSimakov <mishaaniskin@gmail.com>
2024-01-24 19:57:11 +0300

    Added "Partners" block. Styled this one

--- app/books/layout.tsx
+++ app/books/layout.tsx
@@ -11,20 +11,6 @@ const BooksLayout = ({ children }: { children: React.ReactNode }) => {
         <div className={style.leftBar}>
           <Categories />
           <Support />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            nostrum eius reiciendis quis eaque molestias quam quae suscipit
-            illum unde! Maxime doloribus commodi animi soluta nostrum
-            repellendus at molestias nobis, ipsa veritatis ex ipsum, officia
-            tempore! Aperiam consectetur distinctio quidem ducimus optio fugiat
-            porro consequatur ex ipsam laudantium alias cum maiores similique
-            iste quaerat velit dolores eligendi dignissimos, enim modi corporis
-            deleniti. Veniam, asperiores alias nihil odio odit soluta
-            perspiciatis libero tempore ab quod autem ad ratione deleniti
-            eveniet quo. Quaerat reiciendis ducimus eum nisi consequatur id
-            atque dolore obcaecati magni consequuntur. Dolor modi ipsa, velit
-            dicta ipsam earum nihil!
-          </p>
         </div>
         <div className={style.rightBar}>{children}</div>
       </section>
--- components/Support.tsx
+++ components/Support.tsx
@@ -1,5 +1,10 @@
 "use client";
+import { useRef, useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import { IoIosArrowDown } from "react-icons/io";
 import Planeta from "../public/support/Grupo_Planeta_logo.svg";
 import Hachette from "../public/support/Logo_Hachette_Livre_Actuel.svg";
 import Scholastic from "../public/support/Logo_Scholastic.svg";
@@ -15,74 +20,126 @@ const supportList = [
     src: Planeta,
     name: "Planeta",
     id: 1,
-    width: 150,
+    width: 120,
     height: 50,
+    link: "https://www.planeta.es/",
   },
   {
     src: Hachette,
     name: "Hachette",
-    width: 150,
-    height: 50,
+    width: 100,
+    height: 32,
     id: 2,
+    link: "https://www.hachette.de/",
   },
   {
     src: Scholastic,
     width: 200,
-    height: 50,
+    height: 32,
     name: "Scholastic",
     id: 3,
+    link: "https://export.scholastic.com/en",
   },
   {
     src: McGraw,
-    width: 150,
-    height: 50,
+    width: 110,
+    height: 32,
     name: "McGraw",
     id: 4,
+    link: "https://www.mheducation.com/",
   },
   {
     src: Pearson,
     name: "Pearson",
-    width: 150,
-    height: 50,
+    width: 100,
+    height: 32,
     id: 5,
+    link: "https://www.pearson.com/",
   },
   {
     src: Penguin,
-    name: "Penguin",
-    width: 150,
-    height: 50,
+    name: "Penguin random house",
+    width: 60,
+    height: 32,
     id: 6,
+    link: "https://www.penguinrandomhouse.com/",
   },
   {
     src: Reed,
-    name: "Reed",
-    width: 150,
-    height: 30,
+    name: "Reed Elsevier",
+    width: 155,
+    height: 32,
     id: 7,
+    link: "https://www.relx.com/",
   },
   {
     src: Thomson,
-    name: "Thomson",
-    width: 150,
-    height: 23,
+    name: "Thomson Reuters",
+    width: 200,
+    height: 30,
     id: 8,
+    link: "https://www.thomsonreuters.com/en.html",
   },
   {
     src: Wolters,
     name: "Wolters",
     width: 150,
-    height: 33,
+    height: 32,
     id: 9,
+    link: "https://www.wolterskluwer.com/",
   },
 ];
 
+const VerticalSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderRef = useRef<any>(null);
+  const handleNext = () => {
+    setCurrentSlide(currentSlide + 1);
+    sliderRef.current.swiper.slideNext();
+  };
+
+  return (
+    <>
+      <Swiper
+        ref={sliderRef}
+        className="swiper-container support-list"
+        direction="vertical"
+        loop={true}
+        breakpoints={{
+          768: { slidesPerView: 6, slidesPerGroup: 1, rewind: true },
+        }}
+        slidesPerView={5}
+      >
+        {supportList.map(({ id, name, width, height, src, link }, index) => (
+          <SwiperSlide className="support-item" key={id}>
+            <Link
+              rel="noopener noreferrer"
+              target="_blank"
+              className="support-link"
+              href={link}
+            >
+              <span>{`0${index + 1}`}</span>
+              <Image src={src} alt={name} width={width} height={height} />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button className="support-button-next" onClick={handleNext}>
+        <IoIosArrowDown />
+      </button>
+    </>
+  );
+};
+
 const Support = () => {
   return (
-    <ul className="my-slider">
-      {supportList.map(({ id, src, name, width, height }) => (
-        <Image src={src} alt={name} width={width} height={height} />
-      ))}
-    </ul>
+    <section className="support">
+      <div className="support-container">
+        <h2 className="support-title">Our partners</h2>
+
+        <VerticalSlider />
+      </div>
+    </section>
   );
 };
 
